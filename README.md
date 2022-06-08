e160112c57e45943b659e8e3db7c4afbe787e802
Beabaea <102560773+Beabaea@users.noreply.github.com>
2022-06-07 22:12:04 -0700

    added tabs, need to clean up text

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # STAT431-final-shinyapp
  shiny app for STAT431 final project
+
--- app.R
+++ app.R
@@ -17,6 +17,18 @@ D07_LAC <- read.csv(here::here("D07_LAC.csv"))
 D12_RBK <- read.csv(here::here("D12_RBK.csv"))
 D13_MIS <- read.csv(here::here("D13_MIS.csv"))
 
+
+
+#will move into sever, lets have it be reactive to 
+Mutations <- NULL
+for (i in 1:nrow(dat)){ #can't figure this out without a loop but luckily df are small :,)
+  subdat<-dat[i,]%>%select(-c(ID,tot))
+  muts<-vecmutations(subdat)
+  Mutations <- c(Mutations, muts)
+}
+table <- data.frame('ID' = dat$ID, 'Total number of mutations' = dat$tot, 'Mutations' = Mutations)
+
+
 possibledat<- c("D04_LSN","D04_LSR", "D04_RBT",
                "D06_BCC","D06_MIS","D07_LAC",
                "D12_RBK","D13_MIS")
@@ -38,22 +50,51 @@ startplot <- function(dat, hmap = FALSE, phylolayout = 'rectangular'){
     #plot(dathclust)
     phylodat<-as.phylo(dathclust)
     ggtree(phylodat, layout = phylolayout, branch.length = 'none') +
-      geom_tiplab()
+      geom_tiplab() #+ 
+      #ggplot2::xlim(0, 100)
+      
       
   }
-  #return(p)
+}
+
+#very small helper function to create vector of mutations in one sample
+vecmutations <- function(dat){
+  toString(colnames((dat[,colSums(dat) > 0])))
 }
 
 #### Building the App page ####
 
 ui <- fluidPage(
   headerPanel("Heat Map and Dendrogram of Mutations Among Similar Regions"),
-  selectInput('site', 'Sample', possibledat),
-  radioButtons('ptype', 'Plot Type',
-              c("Phylogeny","Heatmap"),
-              selected = "Phylogeny"),
-  uiOutput('phylayout'),
-  plotOutput('plot1')
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('site', 'Sample', possibledat),
+      radioButtons('ptype', 'Plot Type',
+                   c("Phylogeny","Heatmap"),
+                   selected = "Phylogeny"),
+      uiOutput('phylayout')
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plot", plotOutput('plot1')),
+        tabPanel("Data Table",tableOutput('infotab')),
+        tabPanel("General Information",
+                 HTML("This project is by Beatrice Weier. <br> 
+                 These plots take data from 
+                 <a 'https://www.nature.com/articles/s41586-020-2785-8'> this paper </a> ;
+                 an article with data regarding mutations in melanocytes from 
+                 different subject. The R code takes this data and creates a 
+                 purely objective scaling of mutations with equal distances for 
+                 all mutations. A big disclaimer is that the distance are 
+                 calculated from the gene name, not the specific mutation within 
+                 the gene. I hope to eventually better this app and make 
+                 phylogenies accountign for biologicalvariance and probabilites 
+                of certain mutations"))
+      )
+    )
+  )
   
 )
 
@@ -63,7 +104,7 @@ server <- function(input, output) {
   
   output$phylayout <- renderUI({
     if(input$ptype != 'Heatmap'){
-      selectInput('phylayout', "Phylogeny Layout",
+      radioButtons('phylayout', "Phylogeny Layout",
                   playouts,
                   selected = "rectangular")
     }
@@ -81,6 +122,29 @@ server <- function(input, output) {
       startplot(dat(), hmap = TRUE)
     }
   })
+  
+  output$infotab <- renderTable({
+    Mutations <- NULL
+    
+    for (i in 1:nrow(dat())){ #can't figure this out without a loop but luckily df are small :,)
+      subdat<-dat()[i,]%>%select(-c(ID,tot))
+      muts<-vecmutations(subdat)
+      Mutations <- c(Mutations, muts)
+    }
+    
+    data.frame('ID' = dat()$ID, 'Number_of_Mutations' = dat()$tot, 'Genes' = Mutations)
+    
+  })
+  
+  #output$txt <- renderText(
+   # "This project is by Beatrice Weier. These plots take data from https://www.nature.com/articles/s41586-020-2785-8 ;
+    #an article with data regarding mutations in melanocytes from different subject. 
+    #The R code takes this data and creates a purely objective scaling of mutations 
+    #with equal distances for all mutations. A big disclaimer is that the distance 
+    #are calculated from the gene name, not the specific mutation within the gene.
+    #I hope to eventually better this app and make phylogenies accountign for biological
+    #variance and probabilites of certain mutations"
+  #)
 }
 
 shinyApp(ui = ui, server = server)
